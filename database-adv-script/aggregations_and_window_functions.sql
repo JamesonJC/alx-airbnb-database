38d29ea4972e64b43177887e3291f153cf942054
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-05 12:42:45 +0000

    3. Implement Indexes for Optimization

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,4 +2,4 @@ SELECT count(*),
 ROW_NUMBER() OVER (PARTITION BY status_id) AS row_num,
 RANK() OVER (PARTITION BY created_at) AS rank_time
 FROM "Booking"
-GROUP BY "Booking".status_id, "Booking".created_at
+GROUP BY "Booking".status_id, "Booking".created_at""
