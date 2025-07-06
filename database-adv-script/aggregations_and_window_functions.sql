191caea2424d0c07f987558d3df8415839be309e
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-06 17:24:44 +0000

    6. Monitor and Refine Database Performance

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,3 +3,4 @@ ROW_NUMBER() OVER (PARTITION BY status_id) AS row_num,
 RANK() OVER (PARTITION BY created_at) AS rank_time
 FROM "Booking"
 GROUP BY "Booking".status_id, "Booking".created_at""
+["COUNT"]
